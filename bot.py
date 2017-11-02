b8b64e4857418ac4cce1493e3ff5e7cfeb0231fd
boogogeesh <ludlap@yandex.ru>
2017-11-02 22:47:23 +0300

    heroku preparations

--- bot.py
+++ bot.py
@@ -5,15 +5,18 @@ import random
 import re
 
 bot = telebot.TeleBot(config.token)
+
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     time.sleep(1)
     if "пис" in message.text.lower():
-        bot.send_message(message.chat.id, random.choice(['пись', 'пись)', 'пись))','пись))00)0']))
+        bot.send_message(message.chat.id, random.choice(['пись', 'пись)', 'пись))', 'пись))00)0']))
     if "илья" in message.text.lower() and "гей" in message.text.lower():
         bot.send_message(message.chat.id, "нет ты(")
     if re.search(r'м+и+я+г+и+', message.text.lower()):
         bot.send_message(message.chat.id, "мияяяяяги")
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
