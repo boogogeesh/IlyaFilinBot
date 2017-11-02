f20ef3bfe3d7e59d33d1bdb55ff3eb5f8955c13e
maxploded <pro100Giggles@gmail.com>
2017-11-02 17:16:35 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+token = '445485615:AAGPAA1mqEVWZhdRuy4PlC-hnJGpmbv9N6U'
+
+import telebot
+import time
+import random
+
+bot = telebot.TeleBot(token)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    time.sleep(1)
+    if "пис" in message.text.lower():
+        bot.send_message(message.chat.id, random.choice(['пись', 'пись)', 'пись))','пись))00)0']))
+    if "илья" in message.text.lower() and "гей" in message.text.lower():
+        bot.send_message(message.chat.id, "нет ты(")
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
