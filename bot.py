523fff7ef1d183d1e1f86d5b56c7f14902b757c3
maxploded <pro100Giggles@gmail.com>
2017-11-02 17:57:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import config
 import telebot
 import time
 import random
+import re
 
 bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=["text"])
@@ -11,7 +12,7 @@ def repeat_all_messages(message):
         bot.send_message(message.chat.id, random.choice(['пись', 'пись)', 'пись))','пись))00)0']))
     if "илья" in message.text.lower() and "гей" in message.text.lower():
         bot.send_message(message.chat.id, "нет ты(")
-    if "мияги" in message.text.lower():
+    if re.search(r'м+и+я+г+и+', message.text.lower()):
         bot.send_message(message.chat.id, "мияяяяяги")
 
 if __name__ == '__main__':
