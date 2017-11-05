cd9576d282a714aa9f89f335ffb4d4fc8c4a3d0d
boogogeesh <ludlap@yandex.ru>
2017-11-05 16:31:26 +0300

    heroku preparations

--- bot.py
+++ bot.py
@@ -4,22 +4,29 @@ import time
 import random
 import re
 
+from pikabu import get_pikabu_post_link
+
 bot = telebot.TeleBot(config.token)
 
+YOBA_CONFUSED = "CAADAgAD0gMAAgwWHwL2Lqda-jMVBAI"
+YOBA_GLAD = "AAQCABP5yFkqAATiX90FYWOD--MnAAIC"
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler()
 def repeat_all_messages(message):
     time.sleep(1)
     processed_msg = message.text.lower()
     if "пис" in processed_msg:
         bot.send_message(message.chat.id, random.choice(['пись', 'пись)', 'пись))', 'пись))00)0']))
     if "илья" in processed_msg and "гей" in message.text.lower():
-        bot.send_sticker(message.chat.id, "CAADAgAD0gMAAgwWHwL2Lqda-jMVBAI")
+        bot.send_sticker(message.chat.id, YOBA_CONFUSED)
     if re.search(r'м+и+я+г+и+', processed_msg):
         bot.send_message(message.chat.id, "мияяяяяги")
-    if re.search(r'\*илья как дела\*', processed_msg):
-        bot.send_message(message.chat.id, )
-
+    if "как дела" in processed_msg:
+        bot.send_message(message.chat.id, "Хорошо")
+        bot.send_message(message.chat.id, "А у тебя?")
+        bot.send_message(message.chat.id, get_pikabu_post_link())
+        bot.send_sticker(message.chat.id, YOBA_GLAD)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
+
--- pikabu.py
+++ pikabu.py
@@ -1,14 +1,15 @@
+from bs4 import BeautifulSoup
 import random
-
 import requests
-from lxml import html
 
 ENDPOINT = "https://pikabu.ru/"
 
 
-def get_post_link():
+def get_pikabu_post_link():
     posts_type = random.choice(['hot', 'best'])
     page = requests.get(ENDPOINT + posts_type)
-    print(page)
-    tree = html.fromstring(page.content)
-    print(tree)
+    html = BeautifulSoup(page.content, "lxml")
+    posts = html.find_all('a', attrs={'class': "story__title-link "}, href=True)
+    post = random.choice(posts[1:])
+    return post['href']
+
