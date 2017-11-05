9106fbb98a7af2317b75666f75b0165f76a1c821
boogogeesh <ludlap@yandex.ru>
2017-11-05 17:03:20 +0300

    logs

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import telebot
 import time
 import random
 import re
+import sys, traceback
 
 from pikabu import get_pikabu_post_link
 
@@ -14,19 +15,23 @@ YOBA_GLAD = "CAADAgAD6AMAAgwWHwIOqWgUfLdNNQI"
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    time.sleep(1)
-    processed_msg = message.text.lower()
-    if "пис" in processed_msg:
-        bot.send_message(message.chat.id, random.choice(['пись', 'пись)', 'пись))', 'пись))00)0']))
-    if "илья" in processed_msg and "гей" in message.text.lower():
-        bot.send_sticker(message.chat.id, YOBA_CONFUSED)
-    if re.search(r'м+и+я+г+и+', processed_msg):
-        bot.send_message(message.chat.id, "мияяяяяги")
-    if "как дела" in processed_msg:
-        bot.send_message(message.chat.id, "Хорошо")
-        bot.send_message(message.chat.id, "А у тебя?")
-        bot.send_message(message.chat.id, get_pikabu_post_link())
-        bot.send_sticker(message.chat.id, YOBA_GLAD)
+    try:
+        time.sleep(1)
+        processed_msg = message.text.lower()
+        if "пис" in processed_msg:
+            bot.send_message(message.chat.id, random.choice(['пись', 'пись)', 'пись))', 'пись))00)0']))
+        if "илья" in processed_msg and "гей" in message.text.lower():
+            bot.send_sticker(message.chat.id, YOBA_CONFUSED)
+        if re.search(r'м+и+я+г+и+', processed_msg):
+            bot.send_message(message.chat.id, "мияяяяяги")
+        if "как дела" in processed_msg:
+            bot.send_message(message.chat.id, "Хорошо")
+            bot.send_message(message.chat.id, "А у тебя?")
+            bot.send_message(message.chat.id, get_pikabu_post_link())
+            bot.send_sticker(message.chat.id, YOBA_GLAD)
+
+    except:
+        traceback.print_exc(file=sys.stdout)
 
 
 if __name__ == '__main__':
